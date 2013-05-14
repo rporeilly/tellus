7176ab244c954ea81a6c0c2941b4ae154014d3c5
Ryan <oreilly.ryan@gmail.com>
2013-05-13 21:11:40 -0700

    Mockup added

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Tellus</title>
+    <style>
+      body {
+        background: url("images/carbon.png") repeat;
+      }
+      #wrapper {
+        width: 1100px;
+        height: 1000px;
+        background: url("images/body.png") no-repeat;
+        margin: -10px auto;
+      }
+    </style>
+  </head>
+
+	<body>
+    <div id="wrapper">
+      
+    </div>
+	</body>
+
+</html>
+
